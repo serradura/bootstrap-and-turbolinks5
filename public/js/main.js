f44e9b38891e857a708e48c9ba085496513d569b
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2016-12-01 15:57:16 -0200

    Collapse the navbar before a page visit.
    This is necessary because the Bootstrap collapse plugin changes the
    document attributes, and when you visit some page the TL5 will cache
    the current document. So collapse the navbar, will ensure the right
    behaviour when the page was restored from the cache

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,39 @@
+!function() {
+  "use strict";
+  
+  // var TL5_EVENTS = [
+  //   'turbolinks:click',
+  //   'turbolinks:before-visit',
+  //   'turbolinks:visit',
+  //   'turbolinks:request-start',
+  //   'turbolinks:request-end',
+  //   'turbolinks:before-cache',
+  //   'turbolinks:before-render',
+  //   'turbolinks:render',
+  //   'turbolinks:load'
+  // ];
+  //
+  // for (var index in TL5_EVENTS) {
+  //   registerEventListener(TL5_EVENTS[index]);
+  // }
+  
+  function registerEventListener(name, callback) {
+    document.addEventListener(name, function() {
+      console.log('Perform ' + name);
+      
+      if (typeof callback == 'function') {
+        callback.apply(null, arguments);
+      }
+    });
+  }
+
+  registerEventListener('turbolinks:before-visit', function() {
+    var $navbar = $('.navbar-collapse');
+    
+    if ( $navbar.hasClass('in') ) {
+      $navbar.collapse('hide');
+    }
+  });
+}();
+
 Turbolinks.start();
